a8a6ffd04f1d9749bc562240fd32c1ece2705521
MPickfield <mike@select.dev>
2025-02-06 16:14:32 -0500

    improve completions

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,47 @@
 import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import { completeFromList } from "@codemirror/autocomplete"
+import {LRLanguage, LanguageSupport} from "@codemirror/language"
+import {styleTags, tags as t} from "@lezer/highlight"
+import {completeFromList} from "@codemirror/autocomplete"
 
 export const jinja2Language = LRLanguage.define({
-  parser: parser.configure({}),
+  parser: parser.configure({
+    props: [
+      styleTags({
+        Identifier: t.variableName,
+        Boolean: t.bool,
+        String: t.string,
+        Number: t.number,
+        BlockComment: t.blockComment,
+        CodeTag: t.keyword,
+        Comparison: t.compareOperator,
+        Operator: t.operator,
+        Math: t.arithmeticOperator,
+        "Function/Identifier": t.function(t.definition(t.variableName)),
+        "( )": t.paren,
+        "{ }": t.brace,
+        "{{ }} {% %}": t.meta,
+      })
+    ]
+  }),
   languageData: {
-    commentTokens: { block: { open: "{#", close: "#}" } }
+    commentTokens: {block: {open: "{#", close: "#}"}}
   }
 })
 
 export const jinja2Completion = jinja2Language.data.of({
   autocomplete: completeFromList([
-    { label: "extends", type: "keyword" },
-    { label: "from", type: "keyword" },
-    { label: "import", type: "function" },
+    {label: "extends", type: "keyword"},
+    {label: "from", type: "keyword"},
+    {label: "import", type: "function"},
+    {label: "if", type: "keyword"},
+    {label: "elif", type: "keyword"},
+    {label: "else", type: "keyword"},
+    {label: "endif", type: "keyword"},
+    {label: "for", type: "keyword"},
+    {label: "endfor", type: "keyword"},
   ])
 })
 
-export function jinja2 () {
+export function jinja2() {
   return new LanguageSupport(jinja2Language, [jinja2Completion])
 }
